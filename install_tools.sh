afc8eb3d13c634995b892c4aeddb47f1752372b7
yyamada12 <49236849+yyamada12@users.noreply.github.com>
2023-07-12 08:47:43 +0900

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -11,7 +11,7 @@ sudo apt install -y autoconf-archive zlib1g-dev uuid-dev libmnl-dev libuv1-dev l
 bash <(curl -Ss https://my-netdata.io/kickstart.sh) --dont-wait
 
 # alp
-wget https://github.com/tkuchiki/alp/releases/download/v1.0.3/alp_linux_amd64.zip
+wget https://github.com/tkuchiki/alp/releases/download/v1.0.11/alp_linux_amd64.zip
 unzip alp_linux_amd64.zip
 sudo install ./alp /usr/local/bin
 rm alp_linux_amd64.zip alp

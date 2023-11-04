b28d188a095d30b2a9c693367cf215d2e3434194
yyamada12 <12yacropolisy@gmail.com>
2023-11-04 16:21:34 +0900

    fix manage etc nginx file

--- manage_etc_files.sh
+++ manage_etc_files.sh
@@ -5,7 +5,7 @@ set -x
 mkdir -p ~/etc/nginx
 sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.org
 sudo mv /etc/nginx/nginx.conf ~/etc/nginx/nginx.conf
-sudo chmod 666 ~/etc/nginx.conf
+sudo chmod 666 ~/etc/nginx/nginx.conf
 sudo ln -s ~/etc/nginx/nginx.conf /etc/nginx/nginx.conf
 
 # mysql

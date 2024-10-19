ab8df32705c289666b7d658eb0d0a2ea862bc18c
yyamada12 <12yacropolisy@gmail.com>
2024-10-19 16:15:13 +0900

    Update install_tools_arm.sh

--- install_tools_arm.sh
+++ install_tools_arm.sh
@@ -8,7 +8,7 @@ sudo apt install -y --fix-missing htop unzip graphviz
 
 # netdata
 sudo apt install -y autoconf-archive zlib1g-dev uuid-dev libmnl-dev libuv1-dev liblz4-dev libssl-dev libjudy-dev
-bash <(curl -Ss https://my-netdata.io/kickstart.sh) --dont-wait
+bash <(curl -Ss https://get.netdata.cloud/kickstart.sh) --dont-wait
 
 # alp
 wget https://github.com/tkuchiki/alp/releases/download/v1.0.11/alp_linux_arm64.zip
